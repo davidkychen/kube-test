b15b722a20e9cfe871641cfaaf0c7ff40257b9a7
davidkychen <davidchen.kyc@gmail.com>
2020-02-04 13:01:29 -0500

    update main

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 // The simplest possible sbt build file is just one line:
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.11.8"
 // That is, to create a valid sbt build, all you've got to do is define the
 // version of Scala you'd like your project to use.
 
@@ -25,6 +25,7 @@ version := "1.0"
 // You can define other libraries as dependencies in your build like this:
 libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
 libraryDependencies += "io.skuber" %% "skuber" % "2.4.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.3"
 // Here, `libraryDependencies` is a set of dependencies, and by using `+=`,
 // we're adding the cats dependency to the set of dependencies that sbt will go
 // and fetch when it starts up.
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,24 +2,42 @@ import skuber._
 import skuber.json.format._
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import scala.util.{Success, Failure}
+import org.apache.spark.launcher.SparkLauncher
+
+import scala.util.{Failure, Success}
 
 object Test {
   def main(args: Array[String]): Unit = {
-    if(args.length < 1){
-      println("Usage: java -jar this.jar <namespace>")
+    if(args.length < 3){
+      println("Usage: java -jar this.jar <k8s api server> <jar path> <spark image>")
     } else {
-      implicit val system = ActorSystem()
-      implicit val materializer = ActorMaterializer()
-      implicit val dispatcher = system.dispatcher
+//      implicit val system = ActorSystem()
+//      implicit val materializer = ActorMaterializer()
+//      implicit val dispatcher = system.dispatcher
+//
+//      val k8sConfig = K8SConfiguration.parseKubeconfigFile()
+//      val k8s = k8sInit
+//      val listPodsRequest = k8s.listInNamespace[PodList](args(0))
+//      listPodsRequest.onComplete {
+//        case Success(pods) => pods.items.foreach { p => println(p.name) }
+//        case Failure(e) => throw(e)
+//      }
+      val launcher = new SparkLauncher().setDeployMode("cluster")
+                    .setAppName("spark-kube-test")
+                    .setMainClass("ValueZones")
+//                    .setVerbose(true)
+                    .setMaster(args(0))
+                    .setAppResource(args(1))
+                    .setConf("spark.kubernetes.container.image", args(2))
+                    .setConf("spark.kubernetes.authenticate.driver.serviceAccountName","spark")
+                    .setConf("spark.hadoop.fs.AbstractFileSystem.gs.impl","com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
+                    .setConf("spark.kubernetes.container.image.pullPolicy","Always")
+      launcher.addAppArgs("gs://tinderbox/user/david/yellow_tripdata_2018*.csv")
+      launcher.addAppArgs("gs://tinderbox/user/david/green_tripdata_2018*.csv")
+      launcher.addAppArgs("gs://tinderbox/user/david/taxi _zone_lookup.csv")
+      launcher.addAppArgs("gs://tinderbox/user/david/kube-test/result")
+      launcher.startApplication()
 
-      val k8sConfig = K8SConfiguration.parseKubeconfigFile()
-      val k8s = k8sInit
-      val listPodsRequest = k8s.listInNamespace[PodList](args(0))
-      listPodsRequest.onComplete {
-        case Success(pods) => pods.items.foreach { p => println(p.name) }
-        case Failure(e) => throw(e)
-      }
     }
   }
 }
